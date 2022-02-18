52549d2e514411a963bb8258cc929b06c20c3b1d
BryanJackson1 <bryanandaimee@byui.edu>
2022-02-17 20:45:08 -0800

    Add rock

---
+++ rfk/game/casting/rock.py
@@ -0,0 +1,11 @@
+class Rock(Actor):
+    """
+    A destructive actor. Subtracts one from the player when hit
+
+    Attributes:
+        character velocity and score
+    """
+    def __init__(self):
+        """define properties"""
+        
+    
