9bd5181627305bf8ef57a053adff88cfe6122542
Jshelby1985 <48341895+Jshelby1985@users.noreply.github.com>
2022-02-17 22:57:34 -0600

    cleaned gem more to make it shiny

--- rfk/game/casting/gem.py
+++ rfk/game/casting/gem.py
@@ -1,8 +1,8 @@
 class Gem(Actor):
     """
-    An item of cultural or historical interest. 
+    An item of value. 
     
-    The responsibility of an Artifact is to provide a message about itself.
+    The responsibility of an gem is to provide a positive value.
 
     Attributes:
         _message (string): A short description about the artifact.
@@ -10,16 +10,3 @@ class Gem(Actor):
     def __init__(self):
         """define properties"""
         
-    def get_message(self):
-        """Gets the artifact's message.
-        
-        Returns:
-            string: The message.
-        """
-    
-    def set_message(self, message):
-        """Updates the message to the given one.
-        
-        Args:
-            message (string): The given message.
-        """
