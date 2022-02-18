649c02a1fb15c6e433575f7bf3d7327633294ef5
BryanJackson1 <bryanandaimee@byui.edu>
2022-02-17 19:50:36 -0800

    clean out director.py

--- rfk/game/directing/director.py
+++ rfk/game/directing/director.py
@@ -15,8 +15,7 @@ class Director:
             keyboard_service (KeyboardService): An instance of KeyboardService.
             video_service (VideoService): An instance of VideoService.
         """
-        self._keyboard_service = keyboard_service
-        self._video_service = video_service
+
         
     def start_game(self, cast):
         """Starts the game using the given cast. Runs the main game loop.
@@ -24,12 +23,7 @@ class Director:
         Args:
             cast (Cast): The cast of actors.
         """
-        self._video_service.open_window()
-        while self._video_service.is_window_open():
-            self._get_inputs(cast)
-            self._do_updates(cast)
-            self._do_outputs(cast)
-        self._video_service.close_window()
+
 
     def _get_inputs(self, cast):
         """Gets directional input from the keyboard and applies it to the robot.
@@ -37,9 +31,7 @@ class Director:
         Args:
             cast (Cast): The cast of actors.
         """
-        robot = cast.get_first_actor("robots")
-        velocity = self._keyboard_service.get_direction()
-        robot.set_velocity(velocity)        
+    
 
     def _do_updates(self, cast):
         """Updates the robot's position and resolves any collisions with artifacts.
@@ -47,19 +39,7 @@ class Director:
         Args:
             cast (Cast): The cast of actors.
         """
-        banner = cast.get_first_actor("banners")
-        robot = cast.get_first_actor("robots")
-        artifacts = cast.get_actors("artifacts")
-
-        banner.set_text("")
-        max_x = self._video_service.get_width()
-        max_y = self._video_service.get_height()
-        robot.move_next(max_x, max_y)
-        
-        for artifact in artifacts:
-            if robot.get_position().equals(artifact.get_position()):
-                message = artifact.get_message()
-                banner.set_text(message)    
+  
         
     def _do_outputs(self, cast):
         """Draws the actors on the screen.
@@ -67,7 +47,3 @@ class Director:
         Args:
             cast (Cast): The cast of actors.
         """
-        self._video_service.clear_buffer()
-        actors = cast.get_all_actors()
-        self._video_service.draw_actors(actors)
-        self._video_service.flush_buffer()
