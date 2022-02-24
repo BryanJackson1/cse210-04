eede4cc16a1456dd718c48c3a2a88009b1be9e2b
Joe Mac <techncrypt@gmail.com>
2022-02-23 17:23:58 -0700

    Filled in code for Color and Point

--- rfk/game/shared/point.py
+++ rfk/game/shared/point.py
@@ -17,7 +17,8 @@ class Point:
             x (int): The specified x value.
             y (int): The specified y value.
         """
-
+        self._x = x
+        self._y = y
 
     def add(self, other):
         """Gets a new point that is the sum of this and the given one.
@@ -28,7 +29,9 @@ class Point:
         Returns:
             Point: A new Point that is the sum.
         """
-
+        x = self._x + other.get_x()
+        y = self._y + other.get_y()
+        return Point(x, y)
 
     def equals(self, other):
         """Whether or not this Point is equal to the given one.
@@ -39,7 +42,7 @@ class Point:
         Returns: 
             boolean: True if both x and y are equal; false if otherwise.
         """
-
+        return self._x == other.get_x() and self._y == other.get_y()
 
     def get_x(self):
         """Gets the horizontal distance.
@@ -47,7 +50,7 @@ class Point:
         Returns:
             integer: The horizontal distance.
         """
-
+        return self._x
 
     def get_y(self):
         """Gets the vertical distance.
@@ -55,7 +58,7 @@ class Point:
         Returns:
             integer: The vertical distance.
         """
-
+        return self._y
 
     def scale(self, factor):
         """
@@ -67,3 +70,4 @@ class Point:
         Returns:
             Point: A new Point that is scaled.
         """
+        return Point(self._x * factor, self._y * factor)
