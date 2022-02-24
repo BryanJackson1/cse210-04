c622f651dc80036ab04db69b92bb0a985ac8fa41
Joe Mac <techncrypt@gmail.com>
2022-02-23 17:33:00 -0700

    Added Video Service

--- rfk/game/services/keyboard_service.py
+++ rfk/game/services/keyboard_service.py
@@ -1,3 +1,7 @@
+import pyray
+from game.shared.point import Point
+
+#Joe
 class KeyboardService:
     """Detects player input. 
     
@@ -14,6 +18,7 @@ class KeyboardService:
         Args:
             cell_size (int): The size of a cell in the display grid.
         """
+        self._cell_size = cell_size
 
     def get_direction(self):
         """Gets the selected direction based on the currently pressed keys.
@@ -21,3 +26,22 @@ class KeyboardService:
         Returns:
             Point: The selected direction.
         """
+        dx = 0
+        dy = 0
+
+        if pyray.is_key_down(pyray.KEY_LEFT):
+            dx = -1
+        
+        if pyray.is_key_down(pyray.KEY_RIGHT):
+            dx = 1
+        
+        if pyray.is_key_down(pyray.KEY_UP):
+            dy = -1
+        
+        if pyray.is_key_down(pyray.KEY_DOWN):
+            dy = 1
+
+        direction = Point(dx, dy)
+        direction = direction.scale(self._cell_size)
+        
+        return direction
